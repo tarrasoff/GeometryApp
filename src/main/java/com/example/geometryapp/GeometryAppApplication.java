15790835f3c06671eaae17efdd119d5f1fdd0c3d
Alex Tarasov <tarasof.av@gmail.com>
2024-04-15 17:59:41 +0300

    Initial commit

---
+++ src/main/java/com/example/geometryapp/GeometryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.geometryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geometryapp/GeometryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geometryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeometryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
